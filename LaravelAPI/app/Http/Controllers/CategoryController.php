c72500ae18e9a39da5b10850eb90c0b4ff32589c
phuongvu <phuongvx@greenbeli.io>
2022-06-26 18:18:15 +0700

    Create category controller

---
+++ LaravelAPI/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\NameSetting as Name;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    
+}
