749428b7ee39c8da1f0f43560e2ad8b694116d3c
phuongvu <phuongvx@greenbeli.io>
2022-06-27 23:12:41 +0700

    Add category action

--- LaravelAPI/app/Http/Controllers/CategoryController.php
+++ LaravelAPI/app/Http/Controllers/CategoryController.php
@@ -9,5 +9,55 @@ use App\Models\Category;
 
 class CategoryController extends Controller
 {
+    public function InsertCategory(Request $request)
+    {
+        $category_name = $request->category_name;
+        $category_root_name = $request->category_root_name;
+        $status = $request->category_status;
+
+        $category_status = 0;
+        if($status == "on") $category_status = 1;
+
+        DB::select(Name::$InsertCategory."'$category_name','$category_root_name',$category_status,");
+
+        $check =  DB::select(Name::$ProcCheckExistsCategory."'$category_name'");
+
+        if($check > 0) return "Insert ".$category_name." Success !!!";
+        return "Insert ".$category_name." Fail !!!";
+    }
+
+    public function UpdateCategory(Request $request)
+    {
+        $category_name = $request->category_name;
+        $category_root_name = $request->category_root_name;
+        $status = $request->category_status;
+
+        $category_status = 0;
+        if($status == "on") $category_status = 1;
+
+        DB::select(Name::$UpdateCategory."'$category_name','$category_root_name',$category_status,");
+
+        $check =  DB::select(Name::$ProcCheckCategoryUpdate."'$category_name'");
+
+        if($check > 0) return "Update ".$category_name." Success !!!";
+        return "Update ".$category_name." Fail !!!";
+    }
+
+    public function SelectCategoryRoot()
+    {
+        $categories = DB::select(Name::$SelectCategoryRoot);
+        return $categories;
+    }
     
+    public function SelectAllCategory()
+    {
+        $categories = DB::select(Name::$SelectAllCategory);
+        return $categories;
+    }
+
+    public function SelectCategoryByRoot($category_root_name)
+    {
+        $categories = DB::select(Name::$SelectCategoryByRoot."'$category_root_name'");
+        return $categories;;
+    }
 }
