c4cfe12a4b810bc1ed2cbb98e9d00e18f3cb5d57
phuongvu <phuongvx@greenbeli.io>
2022-06-27 23:26:28 +0700

    Add customer model

--- LaravelAPI/app/Models/Customer.php
+++ LaravelAPI/app/Models/Customer.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+    public $customer_id;
+    public $customer_rank_id;
+    public $customer_name;
+    public $customer_email;
+    public $customer_pwd;
+    public $customer_contact;
+    public $customer_dob;
+    public $customer_img_name;
+    public $customer_address;
+    public $gender_type_id;
 }
