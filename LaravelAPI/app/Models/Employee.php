08a835d041ec272beb03ebffe49d2f77def11bc2
phuongvu <phuongvx@greenbeli.io>
2022-06-27 23:58:35 +0700

    Add employee model

--- LaravelAPI/app/Models/Employee.php
+++ LaravelAPI/app/Models/Employee.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+    public $emp_id;
+    public $emp_position_id;
+    public $manager_id;
+    public $emp_dept_no;
+    public $emp_hire_date;
+    public $customer_contact;
+    public $emp_sal;
+    public $emp_comm;
+    public $emp_name;
+    public $emp_email;
+    public $emp_pwd;
+    public $emp_contact;
+    public $emp_dob;
+    public $emp_address;
+    public $emp_status;
+    public $emp_blog_count;
+    public $gender_type_id;
 }
