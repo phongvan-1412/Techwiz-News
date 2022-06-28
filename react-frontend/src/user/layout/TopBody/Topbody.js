ef4374c1fa2eadf3f7691332f98f696da2f6313e
phuongvu <phuongvx@greenbeli.io>
2022-06-28 19:41:43 +0700

    Create top body

---
+++ react-frontend/src/user/layout/TopBody/Topbody.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Consumer } from "../Body/BodySpotLight";
+import { Link } from "react-router-dom";
+
+class TopBody extends Component {
+  render() {
+    return (
+      <Consumer>
+        {(value) => {
+          const { spotlights } = value;
+          const q = [];
+          const ourSpotlights = q.concat(spotlights);
+          ourSpotlights.length = 1;
+          return (
+            <div className="row col-9">
+              <div>
+                <h1>Image</h1>
+              </div>
+              {ourSpotlights.map((spotlight) => (
+                <div className="card card-body mb3 col-md-6">
+                  <h1>
+                    <Link
+                      to="/bodySpotLight"
+                      style={{
+                        cursor: "pointer",
+                        color: "black",
+                      }}
+                    >
+                      {spotlight.blog_title}
+                    </Link>
+                  </h1>
+                </div>
+              ))}
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+export default TopBody;
