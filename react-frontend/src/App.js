add4c4b12ac523fb84a9f896aee2f87080f8771b
phuongvu <phuongvx@greenbeli.io>
2022-06-28 17:17:58 +0700

    Update app.js

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,21 +1,37 @@
-import React, {Component} from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './user/layout/Header';
-import Footer from './user/layout/Footer';
-
-
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  NavLink,
+} from "react-router-dom";
+import Header from "./user/layout/Header";
+import Footer from "./user/layout/Footer";
+import SpotLights from "./user/layout/Body/SpotLights";
+import { Provider } from "./user/layout/Body/BodySpotLight";
 
 class App extends Component {
-  render(){
-    return ( 
-      <Router>
-        <div className="App">
-            <Header />
-
+  render() {
+    return (
+      //<Router>
+      <Provider>
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager"></Header>
+            
+            <div className="container">
+              <Routes>
+              
+                
+              </Routes>
+              <SpotLights />
+            </div>
             <Footer />
-        </div>
-      </Router>
-
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
