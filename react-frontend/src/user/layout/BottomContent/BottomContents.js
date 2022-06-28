f353631b879be9cf1be5ed52bed8148bc5fd113a
phuongvu <phuongvx@greenbeli.io>
2022-06-28 23:10:34 +0700

    Create bottom contents

---
+++ react-frontend/src/user/layout/BottomContent/BottomContents.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import BottomContent from "./BottomContent";
+import { Consumer } from "../Body/BodySpotLight";
+
+class BottomContents extends Component {
+  render() {
+    return (
+      <Consumer>
+        {(value) => {
+          const { spotlights } = value;
+          return (
+            <div className="row col-9">
+              <h1>BottomContents</h1>
+              {spotlights.map((spotlight) => (
+                <BottomContent
+                  key={spotlight.blog_id}
+                  spotlight={spotlight}
+                ></BottomContent>
+              ))}
+              
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+export default BottomContents;
