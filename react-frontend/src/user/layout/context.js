1385612a65da9d51bc01f8ad478efca5e6975b7f
phuongvu <phuongvx@greenbeli.io>
2022-06-29 20:18:00 +0700

    Add fake blog

--- react-frontend/src/user/layout/context.js
+++ react-frontend/src/user/layout/context.js
@@ -13,7 +13,8 @@ export class Provider extends Component {
         blog_day_open: "12/01/1991",
         blog_status: "1",
         blog_link: "abs",
-        blog_img_name: "adf"
+        blog_img_name: "adf",
+        category_name: "category4"
       },
       {
         blog_id: 2,
@@ -23,7 +24,9 @@ export class Provider extends Component {
         blog_day_open: "12/01/2000",
         blog_status: "1",
         blog_link: "aadf",
-        blog_img_name: "adf"
+        blog_img_name: "adf",
+        category_name: "category4"
+
       },
       {
         blog_id: 3,
@@ -33,7 +36,8 @@ export class Provider extends Component {
         blog_day_open: "12/12/2021",
         blog_status: "1",
         blog_link: "abs",
-        blog_img_name: "adf"
+        blog_img_name: "adf",
+        category_name: "category4"
       },
       {
         blog_id: 4,
@@ -43,7 +47,143 @@ export class Provider extends Component {
         blog_day_open: "12/01/2022",
         blog_status: "1",
         blog_link: "abs",
-        blog_img_name: "adf"
+        blog_img_name: "adf",
+        category_name: "category4"
+      },
+      {
+        blog_id: 1,
+        emp_name: "Phuong",
+        blog_title: "FAULKNER FOCUS",
+        blog_content: "Raymond Arroyo: Hunter Biden is the 'Michael Myers of the Biden admin'",
+        blog_day_open: "12/01/1991",
+        blog_status: "1",
+        blog_link: "abs",
+        blog_img_name: "adf",
+        category_name: "category3"
+      },
+      {
+        blog_id: 2,
+        emp_name: "Phuong1",
+        blog_title: "AMERICA'S NEWSROOM",
+        blog_content: "Chad Wolf: We have no deterrence in our immigration system",
+        blog_day_open: "12/01/2000",
+        blog_status: "1",
+        blog_link: "aadf",
+        blog_img_name: "adf",
+        category_name: "category3"
+
+      },
+      {
+        blog_id: 3,
+        emp_name: "Phuong2",
+        blog_title: "FOX & FRIENDS",
+        blog_content: "Rachel Campos-Duffy and Sean Duffy celebrate nephew winning Stanley Cup",
+        blog_day_open: "12/12/2021",
+        blog_status: "1",
+        blog_link: "abs",
+        blog_img_name: "adf",
+        category_name: "category3"
+      },
+      {
+        blog_id: 4,
+        emp_name: "Phuong3",
+        blog_title: "FOX & FRIENDS FIRST",
+        blog_content: "Putin can fund his war machine 'indefinitely' until Biden opens up US oil spigots: Rep. Waltz",
+        blog_day_open: "12/01/2022",
+        blog_status: "1",
+        blog_link: "abs",
+        blog_img_name: "adf",
+        category_name: "category3"
+      },
+      {
+        blog_id: 1,
+        emp_name: "Phuong",
+        blog_title: "FAULKNER FOCUS",
+        blog_content: "Raymond Arroyo: Hunter Biden is the 'Michael Myers of the Biden admin'",
+        blog_day_open: "12/01/1991",
+        blog_status: "1",
+        blog_link: "abs",
+        blog_img_name: "adf",
+        category_name: "category2"
+      },
+      {
+        blog_id: 2,
+        emp_name: "Phuong1",
+        blog_title: "AMERICA'S NEWSROOM",
+        blog_content: "Chad Wolf: We have no deterrence in our immigration system",
+        blog_day_open: "12/01/2000",
+        blog_status: "1",
+        blog_link: "aadf",
+        blog_img_name: "adf",
+        category_name: "category2"
+
+      },
+      {
+        blog_id: 3,
+        emp_name: "Phuong2",
+        blog_title: "FOX & FRIENDS",
+        blog_content: "Rachel Campos-Duffy and Sean Duffy celebrate nephew winning Stanley Cup",
+        blog_day_open: "12/12/2021",
+        blog_status: "1",
+        blog_link: "abs",
+        blog_img_name: "adf",
+        category_name: "category2"
+      },
+      {
+        blog_id: 4,
+        emp_name: "Phuong3",
+        blog_title: "FOX & FRIENDS FIRST",
+        blog_content: "Putin can fund his war machine 'indefinitely' until Biden opens up US oil spigots: Rep. Waltz",
+        blog_day_open: "12/01/2022",
+        blog_status: "1",
+        blog_link: "abs",
+        blog_img_name: "adf",
+        category_name: "category2"
+      },
+      {
+        blog_id: 1,
+        emp_name: "Phuong",
+        blog_title: "FAULKNER FOCUS",
+        blog_content: "Raymond Arroyo: Hunter Biden is the 'Michael Myers of the Biden admin'",
+        blog_day_open: "12/01/1991",
+        blog_status: "1",
+        blog_link: "abs",
+        blog_img_name: "adf",
+        category_name: "category1"
+      },
+      {
+        blog_id: 2,
+        emp_name: "Phuong1",
+        blog_title: "AMERICA'S NEWSROOM",
+        blog_content: "Chad Wolf: We have no deterrence in our immigration system",
+        blog_day_open: "12/01/2000",
+        blog_status: "1",
+        blog_link: "aadf",
+        blog_img_name: "adf",
+        category_name: "category1"
+
+      },
+      {
+        blog_id: 3,
+        emp_name: "Phuong2",
+        blog_title: "FOX & FRIENDS",
+        blog_content: "Rachel Campos-Duffy and Sean Duffy celebrate nephew winning Stanley Cup",
+        blog_day_open: "12/12/2021",
+        blog_status: "1",
+        blog_link: "abs",
+        blog_img_name: "adf",
+        category_name: "category1"
+      },
+      {
+        blog_id: 4,
+        emp_name: "Phuong3",
+        blog_title: "FOX & FRIENDS FIRST",
+        blog_content: "Putin can fund his war machine 'indefinitely' until Biden opens up US oil spigots: Rep. Waltz",
+        blog_day_open: "12/01/2022",
+        blog_status: "1",
+        blog_link: "abs",
+        blog_img_name: "adf",
+        category_name: "category1"
       },
     ],
   };
