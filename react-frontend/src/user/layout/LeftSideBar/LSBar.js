39f8f79d5139d53ca01072bd043cc5fe7ef14b23
phuongvu <phuongvx@greenbeli.io>
2022-06-30 01:36:52 +0700

    Add side bar link

--- react-frontend/src/user/layout/LeftSideBar/LSBar.js
+++ react-frontend/src/user/layout/LeftSideBar/LSBar.js
@@ -2,24 +2,45 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-
 class LSBar extends Component {
   render() {
-    const { blog_id, blog_title, blog_content } = this.props.content;
-
+    const { blog_title, blog_content, category_name } = this.props.content;
+    const title = blog_title.replace(" ", "-");
     return (
-        <div className="row" style={{borderBottom:'solid 1px rgba(0,0,0,0.25)'}}>
-            <Link to='/' className="col-md-4"><h4 style={{fontSize:'13px', fontWeight:'bold',color:'#036'}}>Title</h4></Link> 
-            <ul className="col-md-8" >
-                <li className="" style={{fontSize:'13px', fontWeight:'bold',color:'#036'}}>{blog_title}</li>
-                <li className="" style={{fontSize:'13px'}}>{blog_content}</li>
-            </ul>
-        </div>   
+      <div
+        className="row"
+        style={{ borderBottom: "solid 1px rgba(0,0,0,0.25)" }}
+      >
+        <h4
+          className="col-md-4"
+          style={{ fontSize: "13px", fontWeight: "bold" }}
+        >
+          <Link
+            to={`/${category_name}/${title}`}
+            replace
+            className="nav-link"
+            style={{
+              cursor: "pointer",
+              color: "black",
+            }}
+          >
+            {blog_title}
+          </Link>
+        </h4>
+        <ul className="col-md-8">
+          <li className="" style={{ fontSize: "13px", fontWeight: "bold" }}>
+            {blog_title}
+          </li>
+          <li className="" style={{ fontSize: "13px" }}>
+            {blog_content}
+          </li>
+        </ul>
+      </div>
     );
   }
 }
 LSBar.propTypes = {
   content: PropTypes.object.isRequired,
-}
+};
 
 export default LSBar;
--- react-frontend/src/user/layout/RightSideBar/RSBar.js
+++ react-frontend/src/user/layout/RightSideBar/RSBar.js
@@ -2,25 +2,43 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-
 class RSBar extends Component {
   render() {
-    const { blog_id, blog_title, blog_link } = this.props.content;
-
+    const { blog_title, blog_content, category_name } = this.props.content;
+    const title = blog_title.replace(" ", "-");
     return (
-        <div style={{borderBottom:'solid 1px rgba(0,0,0,0.25)'}}>
-            <ul>
-                <li>
-                  <Link to={blog_title} style={{fontSize:'13px', fontWeight:'bold',color:'blue'}}>{blog_title}</Link></li>
-                <li >
-                  <a href={blog_link} style={{fontSize:'13px',color:'blue'}}>{blog_link}</a></li>
-            </ul>
-        </div>   
+      <div style={{ borderBottom: "solid 1px rgba(0,0,0,0.25)" }}>
+        <ul>
+          <li>
+            <Link
+              to={blog_title}
+              style={{ fontSize: "13px", fontWeight: "bold", color: "blue" }}
+            >
+              {blog_title}
+            </Link>
+          </li>
+          <li>
+            <a href={blog_link} style={{ fontSize: "13px", color: "blue" }}>
+              <Link
+                to={`/${category_name}/${title}`}
+                replace
+                className="nav-link"
+                style={{
+                  cursor: "pointer",
+                  color: "black",
+                }}
+              >
+                {blog_title}
+              </Link>
+            </a>
+          </li>
+        </ul>
+      </div>
     );
   }
 }
 RSBar.propTypes = {
   content: PropTypes.object.isRequired,
-}
+};
 
 export default RSBar;
