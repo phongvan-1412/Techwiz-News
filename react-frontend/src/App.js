3f3fd17177c48fe2670553515d81029433d32936
phuongvu <phuongvx@greenbeli.io>
2022-06-30 01:26:44 +0700

    Update App.js

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {BrowserRouter as Router, Route, Routes, Link, NavLink } from "react-router-dom";
+import {BrowserRouter as Router,} from "react-router-dom";
 import Header from "./user/layout/Header";
 import Footer from "./user/layout/Footer";
-import MainContent from "./user/layout/MainContent/MainContent";
-
 import { Provider } from "../src/user/layout/context";
+import MainRoute from "./user/layout/Router/MainRoute";
 
 class App extends Component {
   render() {
@@ -15,7 +14,7 @@ class App extends Component {
           <div className="App">
             <Header branding="Contact Manager"></Header>
             <div className="container">
-                <MainContent />
+              <MainRoute />
             </div>
             <Footer />
           </div>
