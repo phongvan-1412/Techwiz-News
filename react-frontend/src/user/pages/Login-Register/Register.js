ff096d4a0b659680cde389ff7c1dac29ed12af88
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2022-07-03 23:54:24 +0700

    edit createaccount.js

--- react-frontend/src/user/pages/Login-Register/Register.js
+++ react-frontend/src/user/pages/Login-Register/Register.js
@@ -16,37 +16,39 @@ class Register extends Component{
 
                     <div className="form-group">
                         <div className='mt-4'>
-                            <span className="mb-2 font-weight-bold">Email</span>
+                            <span className="mb-2 register-head-text">Email</span>
                             <input className="form-control login-email" type="email" placeholder="Enter your email"/>
                         </div>
                         <div className='mt-4'>
-                            <span className="mb-2 font-weight-bold">Password</span>
+                            <span className="mb-2 register-head-text">Password</span>
                             <input className="form-control login-password" type="password" placeholder="Enter your password"/>                     
                         </div>
                     </div>
 
-                    <div className="form-footer mt-4 mb-4">
-                        <span className="font-weight-bold">Display Name</span>
+                    <div className="form-footer mt-4 mb-2">
+                        <span className="register-head-text">Display Name</span>
                     </div>
 
-                    <div className="form-footer mt-4 mb-4">
+                    <div className="form-footer">
                         <span className="register-footer-note-text">Enter a unique username to comment on articles</span> 
                     </div>
 
-                    <div className='form-submit text-center'>
-                        <button className="form-control btn btn-secondary btn-login" type="submit">Create Account</button>
-                    </div>
-
-                    <div className='mt-4'>
+                    <div className='mt-2'>
                         <input className="form-control login-email" type="text" placeholder="Display Name"/>
                     </div>
 
                     <div className='mt-4'>
-                        <input type="radio"/>
-                        <span>I have read and agree to the BuffDog News 
-                            <Link to='/' className="term-of-use">Terms of Use</Link>  & <Link to='/' className="privacy-policy">Privacy Policy</Link>
+                        <input type="checkbox"/>
+                        <span> I have read and agree to the BuffDog News 
+                            <Link to='#' className="term-of-use"> Terms of Use</Link>  & <Link to='#' className="privacy-policy">Privacy Policy</Link>
                         </span>
                     </div>
+                    
+                    <div className='form-submit mt-4 text-center'>
+                        <button className="form-control btn btn-secondary btn-login" type="submit">Create Account</button>
+                    </div>
+
+                    
 
                 </div>
                 <div className="col-md-4"></div>
